3fa14403cfe51e6357bbc56735c784afe31d132d
shayal58 <shayalpiya39@gmail.com>
2025-04-26 20:52:42 +0545

    uploading

---
+++ index.js
@@ -0,0 +1,18 @@
+// var lastScrollTop = 0;
+// navbar = document.getElementById("navbar");
+// window.addEventListener("scroll", function () {
+// var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+// if (scrollTop > lastScrollTop) {
+//     navbar.style.top = "-100px";
+// } else {
+//     navbar.style.top = "0";
+//     if (scrollTop === 0) {
+//         navbar.classList.remove("scrolled"); 
+//     } else {
+//         navbar.classList.add("scrolled"); 
+//         }
+//     }
+// lastScrollTop = scrollTop;
+// }
+// );
